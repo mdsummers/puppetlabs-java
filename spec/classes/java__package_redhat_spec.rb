a3ede76216b81b16d2f3f82a8413e34ec282686b
Eric Shamow <eric@puppetlabs.com>
2012-07-12 11:43:07 -0600

    Unit test fixes

--- spec/classes/java__package_redhat_spec.rb
+++ spec/classes/java__package_redhat_spec.rb
@@ -2,13 +2,14 @@ require 'spec_helper'
 
 describe 'java::package_redhat' do
 
-  describe 'should install from params passed with no rpmsource' do
+  describe 'install from params passed with no rpmsource' do
     let(:params) do
       { :version => '1.7.0', :distribution => 'jre' }
     end
     it { should contain_package('java').with({
       :ensure => '1.7.0',
       :name   => 'jre' })}
+      debugger
   end
   describe 'should install from params passed with rpmsource' do
     let(:params) do
